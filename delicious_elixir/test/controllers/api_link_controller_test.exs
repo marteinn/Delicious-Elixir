3e45f2aaee1f53339b271175733a8509666182ef
Martin Sandström <martin@marteinn.se>
2016-10-11 21:13:53 +0200

    Removed unecessary setup in controller test

--- delicious_elixir/test/controllers/api_link_controller_test.exs
+++ delicious_elixir/test/controllers/api_link_controller_test.exs
@@ -5,15 +5,8 @@ defmodule DeliciousElixir.ApiLinkControllerTest do
   @valid_attrs %{url: "some content", title: "A"}
   @invalid_attrs %{}
 
-
-  setup do
-    conn = conn() |> put_req_header("accept", "application/json")
-    {:ok, conn: conn}
-  end
-
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, api_link_path(conn, :index)
     assert json_response(conn, 200) == []
   end
-
 end
