155038bbeb71f3bad806787bb637aeb6730e28b7
Martin Sandström <martin@marteinn.se>
2016-11-09 23:19:15 +0100

    Added stub styling for link item

--- frontend/app/components/LinkItem/LinkItem.js
+++ frontend/app/components/LinkItem/LinkItem.js
@@ -3,14 +3,14 @@ import React from 'react';
 const LinkItem = ({ link }) => {
     return (
         <div className="LinkItem">
-            <h3><a href={link.url}>{link.title}</a> <span>{link.url}</span></h3>
+            <h3 className="LinkItem__Title"><a href={link.url}>{link.title}</a> <span className="LinkItem__DomainTitle">{link.url}</span></h3>
 
             <div className="LinkItem__Footer">
                 <div className="LinkItem__Column">
                     <span className="LinkItem__Saved">0</span>
                     <p className="LinkItem__Tags">Tag</p>
                 </div>
-                <div className="LinkItem__Column">
+                <div className="LinkItem__Column LinkItem__Column--Right">
                     <span className="LinkItem__Action">Share</span>
                     <span className="LinkItem__Action">Edit</span>
                     <span className="LinkItem__Action">Delete</span>
