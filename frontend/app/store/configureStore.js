35ddef6b6eff4efe4734bf27a04d1d71101c9dd1
Martin Sandström <martin@marteinn.se>
2016-11-22 20:44:17 +0100

    Added redux remote devtools

--- frontend/app/store/configureStore.js
+++ frontend/app/store/configureStore.js
@@ -3,15 +3,17 @@ import { routerMiddleware } from 'react-router-redux';
 // import {persistStore, autoRehydrate} from 'redux-persist';
 import { browserHistory } from 'react-router';
 import thunkMiddleware from 'redux-thunk';
+import devToolsEnhancer from 'remote-redux-devtools';
 import rootReducer from '../reducers/index';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const configureStore = (initialState) => {
     const reduxRouterMiddleware = routerMiddleware(browserHistory);
 
     const store = createStore(
         rootReducer,
         initialState,
-        compose(
+        composeEnhancers(
             // autoRehydrate(),
             applyMiddleware(reduxRouterMiddleware, thunkMiddleware)
         )
