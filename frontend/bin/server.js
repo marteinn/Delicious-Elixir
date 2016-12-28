62b51ae98ef7fc392a3f7b51a36cf174e5a1420c
Martin Sandström <martin@marteinn.se>
2016-12-28 21:17:54 +0100

    Added string literal conversion on custom templates

--- frontend/bin/server.js
+++ frontend/bin/server.js
@@ -75,6 +75,10 @@ function allPaths(app) {
             let component = req.path.replace(compPath + '/', '');
             let data = getData(component);
             template = getComponentIndex(component, data);
+            if (template) {
+                template = eval('`'+template+'`');
+            }
+
             if(!template) {
                 template = getTemplate(component, data);
                 snippet = getSnippet(component);
