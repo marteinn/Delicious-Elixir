e0a8840aa42ecfc39baea03706e5a584b451ea7f
Martin Sandström <martin@marteinn.se>
2017-01-05 18:41:51 +0100

    Updated UserHeader styling

--- frontend/app/components/UserHeader/UserHeader.js
+++ frontend/app/components/UserHeader/UserHeader.js
@@ -15,14 +15,15 @@ class UserHeader extends React.Component {
                     <div className="UserHeader__Description">
                         <div className="UserHeader__DescriptionTop">
                             <h2 className="UserHeader__DescriptionUsername">{user.username}</h2>
-                            <span className="UserHeader__Info">{'Description'}</span>
+                            <p className="UserHeader__DescriptionInfo">{'Description'}</p>
+                            <a className="UserHeader__DescriptionUrl" href="#">{'example.com'}</a>
                         </div>
                         <div className="UserHeader__DescriptionBottom">
                             <ul className="UserHeader__Stats">
-                                <li className="UserHeader__StatItem"><strong>{stats.link_count}</strong> Links</li>
-                                <li className="UserHeader__StatItem"><strong>15</strong> Following</li>
-                                <li className="UserHeader__StatItem"><strong>5</strong> Followers</li>
-                                <li className="UserHeader__StatItem"><strong>2</strong> Subscriptions</li>
+                                <li className="UserHeader__StatItem"><strong>{stats.link_count}</strong> links</li>
+                                <li className="UserHeader__StatItem"><strong>15</strong> following</li>
+                                <li className="UserHeader__StatItem"><strong>5</strong> followers</li>
+                                <li className="UserHeader__StatItem"><strong>2</strong> subscriptions</li>
                             </ul>
                             <div className="UserHeader__Joined">
                                 Joined on {user.inserted_at.substr(0, 10)}
